6b47fbb0bda76545480185b8e30c5815f414eec3
Jadey Hagiwara <jadey_hagiwara@brown.edu>
2021-03-19 21:03:48 -0400

    panning working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,56 +5,71 @@ import Map from "./Map"
 
 function App() {
 
-  const [nw, setNW] = useState({latitude: 41.82686216366997, longitude: -71.40645750837325})
-  const [se, setSE] = useState({latitude: 41.82355259953458, longitude: -71.4000750453612})
-  
+  const [nw, setNW] = useState({ latitude: 41.82686216366997, longitude: -71.40645750837325 })
+  const [se, setSE] = useState({ latitude: 41.82355259953458, longitude: -71.4000750453612 })
+
+  const [tempNW, setTempNW] = useState(nw)
+  const [tempSE, setTempSE] = useState(se)
+
+  const dragStartX = useRef(0)
+  const dragStartY = useRef(0)
+
   const canvasRef = useRef(null)
   const isDragging = useRef(false)
 
   const onMouseDown = useCallback(e => {
     if (canvasRef.current && canvasRef.current.contains(e.target)) {
-      console.log("go")
+      //console.log("go")
       isDragging.current = true
+      dragStartX.current = e.pageX
+      dragStartY.current = e.pageY
     }
   }, []);
 
-  const onMouseUp = useCallback(() => {
+  const onMouseUp = useCallback(e => {
     if (isDragging.current) {
       console.log("end")
       isDragging.current = false;
+
+      setNW(nw => {
+        return { latitude: nw.latitude + 0.000005 * -1 * (dragStartY.current - e.pageY), longitude: nw.longitude - 1.928 * 0.000005 * (e.pageX - dragStartX.current) }
+      })
+      setSE(se => {
+        return { latitude: se.latitude + 0.000005 * -1 * (dragStartY.current - e.pageY), longitude: se.longitude - 1.928 * 0.000005 * (e.pageX - dragStartX.current) }
+      })
     }
   }, []);
 
   const onMouseMove = useCallback(e => {
     if (isDragging.current) {
-      console.log(e.movementX)
-      setNW(nw => {
-         return {latitude: nw.latitude + 0.00001*e.movementY , longitude: nw.longitude - 1.928*0.00001*e.movementX}
-      })
-      setSE(se => {
-        return {latitude: se.latitude + 0.00001*e.movementY , longitude: se.longitude - 1.928*0.00001*e.movementX }
-      })
+      //console.log(e.movementX)
+      // setTempNW(nw => {
+      //   return { latitude: nw.latitude + 0.00001 * e.movementY, longitude: nw.longitude - 1.928 * 0.00001 * e.movementX }
+      // })
+      // setTempSE(se => {
+      //   return { latitude: se.latitude + 0.00001 * e.movementY, longitude: se.longitude - 1.928 * 0.00001 * e.movementX }
+      // })
     }
   }, []);
 
   useEffect(() => {
     document.addEventListener("mouseup", onMouseUp);
     document.addEventListener("mousedown", onMouseDown);
-    document.addEventListener("mousemove", onMouseMove);
+    //document.addEventListener("mousemove", onMouseMove);
     return () => {
       document.removeEventListener("mouseup", onMouseUp);
       document.removeEventListener("mousedown", onMouseDown);
-      document.removeEventListener("mousemove", onMouseMove);
+      //document.removeEventListener("mousemove", onMouseMove);
     };
-  }, [onMouseMove, onMouseDown, onMouseUp]);
+  }, [onMouseDown, onMouseUp]);
 
   return (
     <div className="App">
       <header className="App-header">
-        
-        <Route title = "Maps"/>
 
-        <Map corners = {{nw, se}} canvasRef={canvasRef}/>
+        <Route title="Maps" />
+
+        <Map corners={{ nw, se }} canvasRef={canvasRef} />
       </header>
     </div>
   );
--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -5,39 +5,60 @@ import { act } from "react-dom/test-utils"
 const Map = ({ corners, canvasRef }) => {
     const { se, nw } = corners
     const [CANVAS_WIDTH, CANVAS_HEIGHT] = [500, 500]
-    
-    const TILE_ORIGIN = {latitude: 41.82686216366997, longitude: -71.40645750837325}
-    const TILE_WIDTH = -1*(71.4000750453612 -71.40645750837325) / 2
+
+    const TILE_ORIGIN = { latitude: 41.82686216366997, longitude: -71.40645750837325 }
+    const TILE_WIDTH = -1 * (71.4000750453612 - 71.40645750837325) / 2
     const TILE_HEIGHT = (41.82686216366997 - 41.82355259953458) / 2
 
     const [activeWays, setActiveWays] = useState([])
 
     const getTileFromCoords = (latitude, longitude) => {
-        
-        const tileLat = TILE_ORIGIN.latitude + TILE_HEIGHT*Math.floor((latitude - TILE_ORIGIN.latitude) / TILE_HEIGHT)
-        const tileLong = TILE_ORIGIN.longitude + TILE_WIDTH*Math.floor((longitude - TILE_ORIGIN.longitude) / TILE_WIDTH)
 
-        return { latitude: tileLat , longitude: tileLong }
+        const tilesHeight = (latitude - TILE_ORIGIN.latitude) / TILE_HEIGHT
+        const tilesWidth = (longitude - TILE_ORIGIN.longitude) / TILE_WIDTH
+
+        let tileLong
+        let tileLat
+        if (tilesHeight == 0) {
+            tileLong = TILE_ORIGIN.longitude
+        } else {
+            tileLong = TILE_ORIGIN.longitude + TILE_WIDTH * tilesWidth / Math.abs(tilesWidth) * Math.floor(Math.abs(tilesWidth))
+        }
+
+        if (tilesWidth == 0) {
+            tileLat = TILE_ORIGIN.latitude
+        } else {
+            tileLat = TILE_ORIGIN.latitude + TILE_HEIGHT * tilesHeight / Math.abs(tilesHeight) * Math.floor(Math.abs(tilesHeight))
+        }
+
+        return { latitude: tileLat, longitude: tileLong }
     }
 
     const getActiveTiles = () => {
+
         const tileNW = getTileFromCoords(nw.latitude, nw.longitude)
         const tileSE = getTileFromCoords(se.latitude, se.longitude)
 
-        let currentTile = tileNW
+        // console.log(tileNW)
+        // console.log(tileSE)
+        //  console.log("start")
+
+        let currentTile = { latitude: tileNW.latitude + TILE_HEIGHT, longitude: tileNW.longitude - TILE_WIDTH }
 
         const activeTiles = []
 
-        while(currentTile.latitude > tileSE.latitude) { 
-            while(currentTile.longitude < tileSE.longitude){
+        while (currentTile.latitude > tileSE.latitude - TILE_HEIGHT) {
+            while (currentTile.longitude < tileSE.longitude + TILE_WIDTH) {
                 activeTiles.push(currentTile)
-                const newTile = {latitude: currentTile.latitude, longitude: currentTile.longitude + TILE_WIDTH}
-                //activeTiles.push(newTile)
+                const newTile = { latitude: currentTile.latitude, longitude: currentTile.longitude + TILE_WIDTH }
                 currentTile = newTile
             }
-            currentTile.longitude = tileNW.longitude
+            currentTile.longitude = tileNW.longitude - TILE_WIDTH
             currentTile.latitude = currentTile.latitude - TILE_HEIGHT
         }
+
+        // console.log("active tiles set")
+        console.log(activeTiles)
 
         return activeTiles
     }
@@ -48,16 +69,16 @@ const Map = ({ corners, canvasRef }) => {
         const longRange = se.longitude - nw.longitude
 
         const startX = CANVAS_WIDTH * (start.longitude - nw.longitude) / longRange
-        const startY = CANVAS_HEIGHT * -1*(start.latitude - nw.latitude) / latRange
+        const startY = CANVAS_HEIGHT * -1 * (start.latitude - nw.latitude) / latRange
 
         const endX = CANVAS_WIDTH * (end.longitude - nw.longitude) / longRange
-        const endY = CANVAS_HEIGHT * -1*(end.latitude - nw.latitude) / latRange
+        const endY = CANVAS_HEIGHT * -1 * (end.latitude - nw.latitude) / latRange
 
-        return {start: {x: startX, y: startY}, end: {x: endX, y:endY}}
+        return { start: { x: startX, y: startY }, end: { x: endX, y: endY } }
     }
 
     const draw = context => {
-        
+
         context.fillStyle = '#000000'
         context.canvas.width = CANVAS_WIDTH
         context.canvas.height = CANVAS_HEIGHT
@@ -65,39 +86,41 @@ const Map = ({ corners, canvasRef }) => {
     }
 
     const drawWay = (context, way) => {
-        const { start, end } = getPointsFromWay(way.start, way.end); 
+        const { start, end } = getPointsFromWay(way.start, way.end);
         context.moveTo(start.x, start.y)
         context.lineTo(end.x, end.y)
     }
 
     const requestWays = (tile) => {
+
         axios.post(
-            "http://localhost:4567/ways", 
-            {  
-                "nwLat" : tile.latitude,
-                "nwLong" : tile.longitude,
+            "http://localhost:4567/ways",
+            {
+                "nwLat": tile.latitude,
+                "nwLong": tile.longitude,
                 "seLat": tile.latitude - TILE_HEIGHT,
                 "seLong": tile.longitude + TILE_WIDTH
             },
             {
                 headers: {
-                  "Content-Type": "application/json",
-                  'Access-Control-Allow-Origin': '*',
+                    "Content-Type": "application/json",
+                    'Access-Control-Allow-Origin': '*',
                 }
             }
         ).then((res) => {
+            console.log("Requesting tiles: " + tile)
             const ways = res.data.ways
             const tileId = JSON.stringify(tile)
             localStorage.setItem(tileId, JSON.stringify(ways))
             setActiveWays(actWays => {
                 return actWays.concat(ways)
             })
-    
+
             // context.strokeStyle = "#FF0000";
-        
+
             // context.clearRect(0, 0, context.canvas.width, context.canvas.height);
             // context.beginPath()
-        
+
             // ways.map((way) => {
             //     const wayObj = {
             //         start: {latitude: way[0], longitude: way[1]}, 
@@ -109,19 +132,20 @@ const Map = ({ corners, canvasRef }) => {
             // context.stroke();
 
         }).catch((err) => {
-            
+
         })
     }
 
     useEffect(() => {
 
+        setActiveWays([])
         const getWays = () => {
 
-            const tiles = getActiveTiles(); 
+            const tiles = getActiveTiles();
             tiles.forEach((tile) => {
                 const tileId = JSON.stringify(tile)
                 const tileWays = JSON.parse(localStorage.getItem(tileId))
-                if(tileWays){
+                if (tileWays) {
                     setActiveWays(actWays => {
                         return actWays.concat(tileWays)
                     })
@@ -133,22 +157,23 @@ const Map = ({ corners, canvasRef }) => {
         getWays()
     }, [corners])
 
+    //DRAW CANVAS
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
         draw(context)
 
         context.strokeStyle = "#FF0000";
-         
+
         //context.clearRect(0, 0, context.canvas.width, context.canvas.height);
         context.beginPath()
-         
+
         activeWays.map((way) => {
             const wayObj = {
-                start: {latitude: way[0], longitude: way[1]}, 
-                end: {latitude: way[2], longitude: way[3]}
+                start: { latitude: way[0], longitude: way[1] },
+                end: { latitude: way[2], longitude: way[3] }
             }
-            drawWay(context,wayObj)
+            drawWay(context, wayObj)
         })
         context.stroke();
 
@@ -156,7 +181,7 @@ const Map = ({ corners, canvasRef }) => {
 
 
     return (
-        <canvas ref = {canvasRef}/>
+        <canvas ref={canvasRef} />
     )
 }
 
